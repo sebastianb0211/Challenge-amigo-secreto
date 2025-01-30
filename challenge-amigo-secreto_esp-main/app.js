6cabc13fc3b8cdf704b5f961515f81e1d30b0014
Sebastian Betancur Carvalho <sebastianb.0211@gmail.com>
2025-01-30 08:47:15 -0500

    se generan validaciones para cuando el campo esta vacio y cuando el nombre se repite en la lista

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -2,9 +2,23 @@ let listaNombres = []
 function agregarAmigo(){
     let amigo = document.getElementById("amigo").value
     let lista = document.getElementById("listaAmigos")
-    listaNombres.push(amigo)
-     lista.innerHTML  += `<li> <b>${amigo}<b> </li>`
-     document.getElementById("amigo").value = "";
-     console.log(listaNombres)
+    let error = document.querySelector("h3")
+
+    if (amigo == "") {
+        
+        error.innerHTML = "Por favor ingrese algun valor"
+
+    }else if(listaNombres.includes(amigo)){
+
+        error.innerHTML = "Este amigo ya se encuentra en la lista"
+
+    }else{
+        listaNombres.push(amigo)
+        lista.innerHTML  += `<li> <b>${amigo}<b> </li>`
+        document.getElementById("amigo").value = "";
+        console.log(listaNombres)
+        error.innerHTML = "";
+    }
+   
 
 }
