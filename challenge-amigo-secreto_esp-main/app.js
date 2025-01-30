6145f4ad09ffa194b732d97a50feb9e291cb5a9f
Sebastian Betancur Carvalho <sebastianb.0211@gmail.com>
2025-01-30 09:31:54 -0500

    se termina el desafio y se validan todos los posibles fallos tambien terminando la funcion para sortear el amigo secreto

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,8 +1,14 @@
 let listaNombres = []
+let lista = document.getElementById("listaAmigos")
+let errorAmigo = document.getElementById("errorMensaje")
+let nuevoJuego = document.getElementById("nuevoJuego")
+let error = document.querySelector("h3")
+let resultado = document.getElementById("resultado")
+
+nuevoJuego.setAttribute("disabled",true)
 function agregarAmigo(){
     let amigo = document.getElementById("amigo").value
-    let lista = document.getElementById("listaAmigos")
-    let error = document.querySelector("h3")
+    
 
     if (amigo == "") {
         
@@ -11,14 +17,45 @@ function agregarAmigo(){
     }else if(listaNombres.includes(amigo)){
 
         error.innerHTML = "Este amigo ya se encuentra en la lista"
+        limpiar("amigo")
 
     }else{
         listaNombres.push(amigo)
         lista.innerHTML  += `<li> <b>${amigo}<b> </li>`
-        document.getElementById("amigo").value = "";
+        limpiar("amigo")
         console.log(listaNombres)
         error.innerHTML = "";
+        errorAmigo.innerHTML = ""
+        nuevoJuego.removeAttribute("disabled")
     }
    
 
+}
+function sortearAmigo(){
+    let numeroSecreto = Math.floor(Math.random()*listaNombres.length)
+    if (listaNombres.length ===0){
+        
+        errorAmigo.innerHTML = "No hay amigos en la lista"
+        resultado.innerHTML =""
+    }else{
+        resultado.innerHTML =`El Amigo secreto es : ${listaNombres[numeroSecreto]} `
+    lista.innerHTML = "";
+    
+
+    }
+    
+    
+}
+function nuevoIntento(){
+    listaNombres = []
+    lista.innerHTML = ""
+    limpiar("amigo")
+    errorAmigo.innerHTML = ""
+    nuevoJuego.setAttribute("disabled",true)
+    error.innerHTML=""
+    resultado.innerHTML = ""
+    
+}
+function limpiar(texto){
+    document.getElementById(texto).value = "";
 }
